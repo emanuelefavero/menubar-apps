567ed73993baa4f723c15ab38e841f0a0b6579a9
Emanuele Favero <emafaux89@gmail.com>
2025-05-23 22:39:36 +0200

    feat: add skeleton animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <main className='pt-20'>
-      <h1 className='animate-slideDown text-4xl text-pink-500'>Hello</h1>
-      <a href='http://'>Ciao</a>
-    </main>
+    <>
+      <h1 className='animate-skeleton text-4xl text-pink-500'>Hello</h1>
+    </>
   )
 }
