08ff4290fffb50a5ec0e1eab7dca815d12d5fa68
Emanuele Favero <emafaux89@gmail.com>
2025-06-11 16:35:54 +0200

    feat: add title and descrription metadata global variables

---
+++ src/config/metadata.ts
@@ -0,0 +1,5 @@
+export const titlePrefix = 'MenuBar'
+export const titleSuffix = 'Apps'
+export const title = `${titlePrefix} ${titleSuffix}`
+export const description =
+  'Lightweight Mac OS applications that run in the menu bar'
