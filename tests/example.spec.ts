dd10e21564c6a745e504b64d587c41a2a91a39c6
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 18:56:50 +0200

    test: add e2e - h1 has correct text

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,19 @@
+import { heroAppName } from '@/config/hero'
 import { title } from '@/config/metadata'
 import { expect, test } from '@playwright/test'
 
+// ? The title here is the tab title, not the h1
 test('has title', async ({ page }) => {
   await page.goto('/')
 
   await expect(page).toHaveTitle(new RegExp(title, 'i'))
+})
+
+// Check that the h1 has the correct text
+test('h1 has correct text', async ({ page }) => {
+  await page.goto('/')
+
+  const h1 = page.locator('h1')
+
+  await expect(h1).toHaveText(new RegExp(heroAppName, 'i'))
 })
