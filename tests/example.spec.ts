7e3c4dbed58a949288ff9d3b175f5bc13878fcd0
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 19:08:35 +0200

    test: add e2e - has description meta tag

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,22 @@
 import { heroAppName } from '@/config/hero'
-import { title } from '@/config/metadata'
+import { description, title } from '@/config/metadata'
 import { expect, test } from '@playwright/test'
 
-// ? The title here is the tab title, not the h1
+// Check that the page has a title (tab title, not h1)
 test('has title', async ({ page }) => {
   await page.goto('/')
 
   await expect(page).toHaveTitle(new RegExp(title, 'i'))
+})
+
+// Check that the page has a description meta tag
+test('has description meta tag', async ({ page }) => {
+  await page.goto('/')
+  const descriptionMeta = page.locator('meta[name="description"]')
+  await expect(descriptionMeta).toHaveAttribute(
+    'content',
+    new RegExp(description, 'i'),
+  )
 })
 
 // Check that the page has a header
