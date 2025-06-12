a14b59d628fb58090232363867ab2bab4f4f2e1e
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 19:13:32 +0200

    refactor: move metadata tests to own file

---
+++ tests/metadata.test.ts
@@ -0,0 +1,19 @@
+import { description, title } from '@/config/metadata'
+import { expect, test } from '@playwright/test'
+
+// Check that the page has a title (tab title, not h1)
+test('has title', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page).toHaveTitle(new RegExp(title, 'i'))
+})
+
+// Check that the page has a description meta tag
+test('has description meta tag', async ({ page }) => {
+  await page.goto('/')
+  const descriptionMeta = page.locator('meta[name="description"]')
+  await expect(descriptionMeta).toHaveAttribute(
+    'content',
+    new RegExp(description, 'i'),
+  )
+})
