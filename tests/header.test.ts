b1d82e1f0f1f341bc8afd624dc959f2b8749bf86
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 19:29:04 +0200

    refactor: e2e header.test

--- tests/header.test.ts
+++ tests/header.test.ts
@@ -1,10 +1,12 @@
 import { expect, test } from '@playwright/test'
 
-// Check that the page has a header
-test('has header', async ({ page }) => {
-  await page.goto('/')
+// Check that the page has a header element
+test.describe('header presence', () => {
+  test('on homepage', async ({ page }) => {
+    await page.goto('/')
 
-  const header = page.locator('header')
+    const header = page.locator('header')
 
-  await expect(header).toBeVisible()
+    await expect(header).toBeVisible()
+  })
 })
