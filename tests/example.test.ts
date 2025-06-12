08e959bf557d41721dd8f174f75508154923cd05
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 19:17:45 +0200

    refactor: move h1 tests to own file

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,35 +1,7 @@
-import { heroAppName } from '@/config/hero'
 import { expect, test } from '@playwright/test'
 
 // Check that the page has a header
 test('has header', async ({ page }) => {
-  await page.goto('/')
-
-  const header = page.locator('header')
-
-  await expect(header).toBeVisible()
-})
-
-// Check that the page has only one h1
-test('has one h1', async ({ page }) => {
-  await page.goto('/')
-
-  const h1s = page.locator('h1')
-
-  await expect(h1s).toHaveCount(1)
-})
-
-// Check that the h1 has the correct text
-test('h1 has correct text', async ({ page }) => {
-  await page.goto('/')
-
-  const h1 = page.locator('h1')
-
-  await expect(h1).toHaveText(new RegExp(heroAppName, 'i'))
-})
-
-// Check that the page has an header element
-test('has header element', async ({ page }) => {
   await page.goto('/')
 
   const header = page.locator('header')
