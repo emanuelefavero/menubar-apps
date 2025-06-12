0d514f1c7cb1ef6933d50498c95af2c42bc0c797
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 18:57:32 +0200

    test: add e2e - has one h1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,6 +18,15 @@ test('has header', async ({ page }) => {
   await expect(header).toBeVisible()
 })
 
+// Check that the page has only one h1
+test('has one h1', async ({ page }) => {
+  await page.goto('/')
+
+  const h1s = page.locator('h1')
+
+  await expect(h1s).toHaveCount(1)
+})
+
 // Check that the h1 has the correct text
 test('h1 has correct text', async ({ page }) => {
   await page.goto('/')
