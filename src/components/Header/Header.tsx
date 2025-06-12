3d2e3ac6fe066bcb4db2f852e3ae29ff5f2471cb
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 16:18:05 +0200

    fix: header position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Menu from './Menu'
 
 export default function Component() {
   return (
-    <header className='flex items-center justify-between px-2 py-[0.2rem]'>
+    <header className='absolute top-0 z-50 flex w-full items-center justify-between px-2 py-[0.2rem] backdrop-blur-xl'>
       <Logo />
       <Menu />
     </header>
