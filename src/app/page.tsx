9a4405d3d8dab21b705479ce4dfbecd76158a2c3
Emanuele Favero <emafaux89@gmail.com>
2025-06-12 15:47:35 +0200

    feat: add hero and card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from '@/components/Hero/Hero'
 import { title } from '@/config/metadata'
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <>
       <h1 className='text-4xl'>{title}</h1>
 
-      <div className='card'></div>
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero/Card.tsx
@@ -0,0 +1,5 @@
+import './Card.css'
+
+export default function Component() {
+  return <div className='card'>Card</div>
+}
