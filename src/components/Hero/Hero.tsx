4956ff1cd304364f98e7b2c15b7dadf2b83547a5
Emanuele Favero <emafaux89@gmail.com>
2025-06-13 20:55:54 +0200

    refactor: move hero css to tailwind

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import './Hero.css'
 
 export default function Component() {
   return (
-    <section className='hero'>
+    <section className='hero relative flex min-h-screen items-center justify-center overflow-hidden pt-12'>
       <Card />
     </section>
   )
