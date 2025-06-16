0fae77822187451160a8cc1662e12dceb3034546
Emanuele Favero <emafaux89@gmail.com>
2025-06-16 17:08:21 +0200

    feat: add text-shadow to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,12 @@ import './not-found.css'
 
 export default function NotFound() {
   return (
-    <div className='not-found relative flex min-h-screen flex-col items-center justify-center gap-4 overflow-hidden pt-12 text-center'>
-      <h1 className='flex flex-col items-center gap-4 text-4xl font-bold'>
+    <div className='not-found relative flex min-h-screen flex-col items-center justify-center gap-4 overflow-hidden pt-12 text-center text-white'>
+      <h1 className='flex flex-col items-center gap-4 text-4xl font-bold text-shadow-(--text-shadow-lg)'>
         <span className='text-4xl font-medium'>404</span>
         Page Not Found
       </h1>
-      <p className='text-lg'>
+      <p className='text-lg text-shadow-(--text-shadow-sm)'>
         Sorry, the page you are looking for does not exist.
       </p>
       <LinkButton>← Go back to Home</LinkButton>
