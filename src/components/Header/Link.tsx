19f73e4a90cdc91286cfa4661cf77548501dbce9
Emanuele Favero <emafaux89@gmail.com>
2025-06-16 22:49:18 +0200

    feat: update the style of header links

--- src/components/Header/Link.tsx
+++ src/components/Header/Link.tsx
@@ -17,9 +17,15 @@ export default function Component({ href, children, className = '' }: Props) {
   return (
     <Link
       href={href}
-      className={`rounded-full px-3 py-1 text-gray-700 no-underline hover:bg-white/20 ${
-        isActive && 'bg-white/20'
-      } group-has-[:hover]:bg-transparent ${className}`}
+      className={`rounded-full px-3 py-1 text-gray-700 no-underline select-none hover:bg-white/30 ${
+        isActive
+          ? 'bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:bg-white/80'
+          : ''
+      } group-has-[:hover]:bg-transparent group-has-[:hover]:shadow-none group-has-[:hover]:inset-shadow-none ${
+        isActive
+          ? 'group-has-[:hover]:hover:bg-white/80 group-has-[:hover]:hover:shadow-2xs group-has-[:hover]:hover:inset-shadow-xs'
+          : ''
+      } ${className} `}
     >
       {children}
     </Link>
--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -9,8 +9,10 @@ export default function Component() {
   return (
     <Link
       href='/'
-      className={`rounded-full px-3 py-0.5 font-semibold text-gray-700 no-underline select-none hover:bg-white/20 ${
-        isHome && 'bg-white/20'
+      className={`rounded-full px-3 py-0.5 font-semibold text-gray-700 no-underline select-none ${
+        isHome
+          ? 'bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:bg-white/80'
+          : 'hover:bg-white/30'
       }`}
     >
       {title}
