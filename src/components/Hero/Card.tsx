fae3e06dcc12f0453023cea7e75f0cc6c01affad
Emanuele Favero <emafaux89@gmail.com>
2025-06-16 16:41:56 +0200

    refactor: tailwind custom properties syntax

--- src/components/Hero/Card.tsx
+++ src/components/Hero/Card.tsx
@@ -3,7 +3,7 @@ import './Card.css'
 
 export default function Component() {
   return (
-    <div className='card relative flex min-h-[480px] w-full max-w-[440px] items-center justify-center rounded-[2.5rem] bg-[var(--card-background)] text-center shadow-[var(--card-shadow)] backdrop-blur-[6px] select-none'>
+    <div className='card relative flex min-h-[480px] w-full max-w-[440px] items-center justify-center rounded-[2.5rem] bg-(--card-background) text-center shadow-(--card-shadow) backdrop-blur-[6px] select-none'>
       <h1 className='text-4xl'>{heroAppName}</h1>
     </div>
   )
