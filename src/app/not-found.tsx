6338995b9f4022dd5ddbf11bc0cdac99cd1833ea
Emanuele Favero <emafaux89@gmail.com>
2025-06-16 18:36:02 +0200

    feat: use Image component for background image in not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,26 @@
 import LinkButton from '@/components/shared/LinkButton'
-import './not-found.css'
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
-    <div className='not-found relative flex min-h-screen flex-col items-center justify-center gap-4 overflow-hidden pt-12 text-center text-white'>
+    <div className='relative flex min-h-screen flex-col items-center justify-center gap-4 overflow-hidden pt-12 text-center text-white'>
+      <Image
+        src='/paul-pastourmatzis-unsplash.jpg'
+        alt='Background'
+        fill
+        className='-z-10 object-cover object-center'
+        priority
+      />
+
       <h1 className='flex flex-col items-center gap-4 text-4xl font-bold text-shadow-(--text-shadow-lg)'>
         <span className='text-4xl font-medium'>404</span>
         Page Not Found
       </h1>
+
       <p className='text-lg text-shadow-(--text-shadow-sm)'>
         Sorry, the page you are looking for does not exist.
       </p>
+
       <LinkButton>← Go back to Home</LinkButton>
     </div>
   )
