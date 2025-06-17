7ffefa9453cae5669df977fce82fabb89b8a36e8
Emanuele Favero <emafaux89@gmail.com>
2025-06-17 12:21:30 +0200

    feat: add attribution link to hero bg image

--- src/components/Hero/Card.tsx
+++ src/components/Hero/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Component({ className = '', children }: Props) {
   return (
     <div
-      className={`card relative flex min-h-[480px] w-full max-w-[440px] items-center justify-center rounded-[2.5rem] bg-(--card-background) text-center text-white shadow-(--card-shadow) backdrop-blur-[6px] select-none ${className}`}
+      className={`card relative z-10 flex min-h-[480px] w-full max-w-[440px] items-center justify-center rounded-[2.5rem] bg-(--card-background) text-center text-white shadow-(--card-shadow) backdrop-blur-[6px] select-none ${className}`}
     >
       {children}
     </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import BackgroundImage from '@/components/shared/BackgroundImage'
 import { heroAppName } from '@/config/hero'
+import Link from 'next/link'
 import Card from './Card'
 
 export default function Component() {
@@ -14,6 +15,17 @@ export default function Component() {
       <Card>
         <h1 className='text-4xl'>{heroAppName}</h1>
       </Card>
+
+      <div className='absolute right-4 bottom-4 rounded-full text-[0.6rem] text-white/70'>
+        <span>Photo by</span>{' '}
+        <Link
+          href='https://unsplash.com/photos/silhouette-of-trees-covered-by-fog-KT3WlrL_bsg'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Paul Pastourmatzis
+        </Link>
+      </div>
     </section>
   )
 }
