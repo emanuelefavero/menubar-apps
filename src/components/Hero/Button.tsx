5158cb229711f62fd8f91b85f96d0c4b09272695
Emanuele Favero <emafaux89@gmail.com>
2025-06-18 22:36:26 +0200

    feat: add clsx to Hero Button

--- src/components/Hero/Button.tsx
+++ src/components/Hero/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 import { AnchorHTMLAttributes } from 'react'
 
@@ -15,16 +16,20 @@ export default function Component({
   className = '',
   ...props
 }: Props) {
+  const baseClasses =
+    'inline-block w-full rounded-full px-4 py-2 text-lg font-medium no-underline transition duration-250 active:scale-[97%]'
+
+  const variantClasses = clsx({
+    'bg-white/80 text-gray-800 shadow-2xs inset-shadow-sm shadow-black/25 inset-shadow-white hover:bg-white':
+      variant === 'primary',
+    'border-b border-white/20 bg-black/10 text-white shadow-md inset-shadow-[0_2px_2px_0_rgba(0,0,0,0.6)] inset-shadow-black/60 hover:bg-gray-500/5':
+      variant === 'secondary',
+  })
+
   return (
     <Link
       href={href}
-      className={`inline-block w-full rounded-full px-4 py-2 text-lg font-medium no-underline transition duration-250 active:scale-[97%] ${
-        variant === 'primary'
-          ? // Primary
-            'bg-white/80 text-gray-800 shadow-2xs inset-shadow-sm shadow-black/25 inset-shadow-white hover:bg-white'
-          : // Secondary
-            'border-b border-white/20 bg-black/10 text-white shadow-md inset-shadow-[0_2px_2px_0_rgba(0,0,0,0.6)] inset-shadow-black/60 hover:bg-gray-500/5'
-      } ${className}`}
+      className={clsx(baseClasses, variantClasses, className)}
       {...props}
     >
       {children}
