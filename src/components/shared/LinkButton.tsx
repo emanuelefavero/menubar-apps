a0b81ed9a0fde12f3a4e88b78317d6dfb2a2882e
Emanuele Favero <emafaux89@gmail.com>
2025-06-18 23:47:43 +0200

    refactor: shared LinkButton with clsx

--- src/components/shared/LinkButton.tsx
+++ src/components/shared/LinkButton.tsx
@@ -1,15 +1,26 @@
+import clsx from 'clsx'
 import Link from 'next/link'
+import { AnchorHTMLAttributes } from 'react'
 
-interface Props {
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
   children?: React.ReactNode
   className?: string
 }
 
-export default function Component({ children, className }: Props) {
+export default function Component({ children, className, ...props }: Props) {
+  const insetShadow = 'inset-shadow-[1px_2px_1px_0_rgba(220,226,232,0.23)]'
+
+  const baseStyles = `rounded-full border border-black/25 bg-(--primary) px-3 py-1.5 text-white no-underline ${insetShadow} transition-all duration-200 select-none text-shadow-(--text-shadow-sm)`
+
+  const hoverStyles = 'hover:bg-(--primary-hover)'
+
+  const activeStyles = 'active:scale-95'
+
   return (
     <Link
       href='/'
-      className={`rounded-full border border-black/25 bg-(--primary) px-3 py-1.5 text-white no-underline inset-shadow-[1px_2px_1px_0_rgba(220,226,232,0.23)] transition-all duration-200 select-none text-shadow-(--text-shadow-sm) hover:bg-(--primary-hover) active:scale-95 ${className}`}
+      className={clsx(baseStyles, hoverStyles, activeStyles, className)}
+      {...props}
     >
       {children}
     </Link>
