b81da2ea0963ac396f5d4d7fb03bf91dad2b8e69
Emanuele Favero <emafaux89@gmail.com>
2025-06-19 19:52:23 +0200

    fix: header content shifting

--- src/components/Header/Link.tsx
+++ src/components/Header/Link.tsx
@@ -21,13 +21,13 @@ export default function Component({
   const isActive = pathname === href
 
   const baseStyles =
-    'rounded-full px-3 py-0.5 text-gray-700 no-underline select-none hover:bg-white/30 transition duration-250'
+    'rounded-full px-3 py-0.5 border-b-2 border-transparent text-gray-700 no-underline select-none hover:bg-white/30 transition duration-250'
 
   const groupStyles =
-    'group-has-[:hover]:border-transparent group-has-[:hover]:bg-transparent group-has-[:hover]:shadow-none group-has-[:hover]:inset-shadow-none'
+    'group-has-[:hover]:!border-transparent group-has-[:hover]:bg-transparent group-has-[:hover]:shadow-none group-has-[:hover]:inset-shadow-none'
 
   const activeStyles =
-    'border-b-2 border-black/15 bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:border-black/15 hover:bg-white/80'
+    'border-b-2 !border-black/15 bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:!border-black/15 hover:bg-white/80'
 
   const activeGroupStyles =
     'group-has-[:hover]:hover:border-black/15 group-has-[:hover]:hover:bg-white/80 group-has-[:hover]:hover:shadow-2xs group-has-[:hover]:hover:inset-shadow-xs'
--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -14,10 +14,10 @@ export default function Component({ className, ...props }: Props) {
   const isHome = useIsHome()
 
   const baseStyles =
-    'rounded-full px-3 py-0.5 font-semibold text-gray-700 no-underline select-none transition duration-250'
+    'rounded-full px-3 py-0.5 font-semibold border-b-2 border-transparent text-gray-700 no-underline select-none transition duration-250'
 
   const homeStyles =
-    'border-b-2 border-black/15 bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:bg-white/80'
+    'border-b-2 !border-black/15 bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:bg-white/80'
 
   const nonHomeStyles = 'hover:bg-white/30'
 
