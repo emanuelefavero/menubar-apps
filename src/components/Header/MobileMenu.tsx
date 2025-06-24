24ef66659f1b108c7163f167cbef8b136451ff1c
Emanuele Favero <emafaux89@gmail.com>
2025-06-24 23:00:10 +0200

    feat: add HamburgerButton, HamburgerMenu components

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -9,50 +9,71 @@ export default function Component() {
 
   return (
     <nav>
-      {/* Hamburger button */}
-      <button
-        className='block cursor-pointer p-1 xs:hidden'
-        aria-label='Open menu'
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        {/* Hamburger button lines as X when open */}
-        <span
-          className={`mb-1 block h-0.5 w-6 rounded-full transition duration-300 ${
-            open ? 'translate-y-1.5 rotate-45 bg-red-800' : 'bg-gray-700'
-          }`}
-        ></span>
-        <span
-          className={`mb-1 block h-0.5 w-6 rounded-full bg-gray-700 transition-opacity duration-150 ${
-            open ? 'opacity-0' : ''
-          }`}
-        ></span>
-        <span
-          className={`block h-0.5 w-6 rounded-full transition duration-300 ${
-            open ? '-translate-y-1.5 -rotate-45 bg-red-800' : 'bg-gray-700'
-          }`}
-        ></span>
-      </button>
-
-      {/* Hamburger Menu */}
-      <div
-        className={`${
-          open ? 'block' : 'hidden'
-        } absolute top-12 left-0 w-full bg-white/90 shadow-md backdrop-blur-lg xs:hidden`}
-      >
-        <ul className='flex flex-col items-center p-4'>
-          {headerLinks.map((link) => (
-            <li key={link.href} className='mb-2'>
-              <Link
-                href={link.href}
-                className='text-lg font-medium text-black hover:text-gray-700'
-                onClick={() => setOpen(false)} // Close menu on link click
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <HamburgerButton open={open} setOpen={setOpen} />
+      <HamburgerMenu open={open} setOpen={setOpen} />
     </nav>
+  )
+}
+
+// -----------
+
+interface HamburgerButtonProps {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function HamburgerButton({ open, setOpen }: HamburgerButtonProps) {
+  return (
+    <button
+      className='block cursor-pointer p-1 xs:hidden'
+      aria-label='Open menu'
+      title='Open menu'
+      onClick={() => setOpen((prev) => !prev)}
+    >
+      <span
+        className={`mb-1 block h-0.5 w-6 rounded-full transition duration-300 ${
+          open ? 'translate-y-1.5 rotate-45 bg-red-800' : 'bg-gray-700'
+        }`}
+      ></span>
+      <span
+        className={`mb-1 block h-0.5 w-6 rounded-full bg-gray-700 transition-opacity duration-150 ${
+          open ? 'opacity-0' : ''
+        }`}
+      ></span>
+      <span
+        className={`block h-0.5 w-6 rounded-full transition duration-300 ${
+          open ? '-translate-y-1.5 -rotate-45 bg-red-800' : 'bg-gray-700'
+        }`}
+      ></span>
+    </button>
+  )
+}
+
+interface HamburgerMenuProps {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function HamburgerMenu({ open, setOpen }: HamburgerMenuProps) {
+  return (
+    <div
+      className={`${
+        open ? 'block' : 'hidden'
+      } absolute top-12 left-0 w-full bg-white/90 shadow-md backdrop-blur-lg xs:hidden`}
+    >
+      <ul className='flex flex-col items-center p-4'>
+        {headerLinks.map((link) => (
+          <li key={link.href} className='mb-2'>
+            <Link
+              href={link.href}
+              className='text-lg font-medium text-black hover:text-gray-700'
+              onClick={() => setOpen(false)} // Close menu on link click
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
