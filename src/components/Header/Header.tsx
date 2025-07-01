669b15a54a64499e99bd7d7493814281d48d8bad
Emanuele Favero <emafaux89@gmail.com>
2025-07-01 21:58:22 +0200

    feat: add slower header appear animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Component() {
   // TIP: We need to add `backdrop-blur` in the `before` pseudo element because the `MobileMenu` will also have a `backdrop-blur` effect, and nested backdrop-blur effects are not supported in CSS. @see https://github.com/tailwindlabs/tailwindcss/discussions/15103
 
   return (
-    <header className='absolute top-0 z-50 w-full'>
+    <header className='absolute top-0 z-50 w-full animate-appear-slower'>
       <div className='relative flex items-center justify-between px-2 py-[0.2rem] before:absolute before:inset-0 before:-z-10 before:backdrop-blur-xl'>
         <BackgroundShadow bgColor='bg-white/30' blur='blur-xl' />
         <Logo />
