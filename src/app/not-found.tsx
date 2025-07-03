ef9a167f61bad6d768ed40391546aaae5e1944bf
Emanuele Favero <emafaux89@gmail.com>
2025-07-04 00:48:25 +0200

    refactor: move TestScroll to components/__dev__

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,31 +1,10 @@
+import TestScroll from '@/components/__dev__/TestScroll'
 import BackgroundImage from '@/components/shared/BackgroundImage'
 import BackgroundShadow from '@/components/shared/BackgroundShadow'
 import LinkAttribution from '@/components/shared/LinkAttribution'
 import LinkButton from '@/components/shared/LinkButton'
 
-// TODO: Remove TestScroll component used for testing scrolling effect
-
-function TestScroll() {
-  return (
-    <div className='flex min-h-screen w-full items-center justify-center bg-(--background) p-20 text-2xl'>
-      <h2>Lorem ipsum dolor sit amet.</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-        dignissimos! Esse quis cum quae harum dicta, corporis et, quam odit, ut
-        aspernatur magni illum quibusdam tempora molestiae omnis suscipit eius
-        commodi impedit? Consectetur itaque doloribus molestias voluptate
-        sapiente sed aliquid repellat fugit quasi accusamus aspernatur corporis
-        soluta consequuntur eum reprehenderit perspiciatis fugiat blanditiis
-        tenetur, deleniti quibusdam nesciunt vero laudantium cumque? Nisi
-        accusantium pariatur odio dignissimos tempora quae facilis magnam
-        eveniet magni illum voluptas saepe perspiciatis maiores quaerat,
-        asperiores itaque sunt omnis quo reiciendis optio quos provident rem
-        nihil. Dolor possimus inventore minus. Voluptatum quaerat placeat, quas
-        architecto iusto sequi voluptatibus?
-      </p>
-    </div>
-  )
-}
+// TODO: Remove TestScroll component used for testing scrolling effect ↓
 
 export default function NotFound() {
   return (
@@ -62,8 +41,13 @@ export default function NotFound() {
         />
       </div>
 
-      <TestScroll />
-      <TestScroll />
+      {/* This is a test scroll component for development purposes */}
+      {process.env.NODE_ENV === 'development' && (
+        <>
+          <TestScroll />
+          <TestScroll />
+        </>
+      )}
     </>
   )
 }
---
+++ src/components/__dev__/TestScroll.tsx
@@ -0,0 +1,23 @@
+// NOTE: This component is just for testing scrolling effect in pages that are still too short
+
+export default function Component() {
+  return (
+    <div className='flex min-h-screen w-full items-center justify-center bg-(--background) p-20 text-2xl'>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+        dignissimos! Esse quis cum quae harum dicta, corporis et, quam odit, ut
+        aspernatur magni illum quibusdam tempora molestiae omnis suscipit eius
+        commodi impedit? Consectetur itaque doloribus molestias voluptate
+        sapiente sed aliquid repellat fugit quasi accusamus aspernatur corporis
+        soluta consequuntur eum reprehenderit perspiciatis fugiat blanditiis
+        tenetur, deleniti quibusdam nesciunt vero laudantium cumque? Nisi
+        accusantium pariatur odio dignissimos tempora quae facilis magnam
+        eveniet magni illum voluptas saepe perspiciatis maiores quaerat,
+        asperiores itaque sunt omnis quo reiciendis optio quos provident rem
+        nihil. Dolor possimus inventore minus. Voluptatum quaerat placeat, quas
+        architecto iusto sequi voluptatibus?
+      </p>
+    </div>
+  )
+}
