63dba83d0c56b1a571b4c728748c2d81b854938e
Emanuele Favero <emafaux89@gmail.com>
2025-07-03 11:38:40 +0200

    feat: change desktop menu link to white on scroll

--- src/components/Header/Menu/Desktop/Link.tsx
+++ src/components/Header/Menu/Desktop/Link.tsx
@@ -27,11 +27,15 @@ export default function Component({
     'rounded-full px-3 py-0.5 border-b-2 border-transparent no-underline select-none hover:bg-white/30 transition duration-250',
   )
 
-  const groupStyles =
-    'group-has-[:hover]:border-transparent! group-has-[:hover]:bg-transparent group-has-[:hover]:shadow-none group-has-[:hover]:inset-shadow-none'
+  const groupStyles = clsx(
+    scrolledHalfway
+      ? 'group-has-[:hover]:text-white!'
+      : 'group-has-[:hover]:text-gray-700!',
+    'group-has-[:hover]:border-transparent! group-has-[:hover]:bg-transparent group-has-[:hover]:shadow-none group-has-[:hover]:inset-shadow-none',
+  )
 
   const activeStyles =
-    'border-b-2 border-black/15! bg-white/80 shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:border-black/15! hover:bg-white/80'
+    'border-b-2 border-black/15! bg-white/80 text-gray-700! shadow-2xs inset-shadow-xs shadow-black/25 inset-shadow-white hover:border-black/15! hover:bg-white/80'
 
   const activeGroupStyles =
     'group-has-[:hover]:hover:border-black/15 group-has-[:hover]:hover:bg-white/80 group-has-[:hover]:hover:shadow-2xs group-has-[:hover]:hover:inset-shadow-xs'
