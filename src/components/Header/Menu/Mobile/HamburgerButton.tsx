79d66dffff728be547dbf30984df05906a084907
Emanuele Favero <emafaux89@gmail.com>
2025-07-03 19:09:54 +0200

    feat: change hamburger button to white on scroll

--- src/components/Header/Menu/Mobile/HamburgerButton.tsx
+++ src/components/Header/Menu/Mobile/HamburgerButton.tsx
@@ -1,3 +1,4 @@
+import { useScrolledHalfway } from '@/hooks/useScrolledHalfway'
 import clsx from 'clsx'
 import { Ref } from 'react'
 
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export default function Component({ open, onClick, ref }: Props) {
+  const scrolledHalfway = useScrolledHalfway()
+
   return (
     <button
       ref={ref}
@@ -18,22 +21,23 @@ export default function Component({ open, onClick, ref }: Props) {
     >
       <span
         className={clsx(
-          'mb-1 block h-0.5 w-6 rounded-full transition duration-300',
+          'mb-1 block h-0.5 w-6 rounded-full transition-transform duration-300',
           open && 'translate-y-1.5 rotate-45 bg-red-800',
-          !open && 'bg-gray-700',
+          !open && scrolledHalfway ? 'bg-white' : 'bg-gray-700',
         )}
       ></span>
       <span
         className={clsx(
-          'mb-1 block h-0.5 w-6 rounded-full bg-gray-700 transition-opacity duration-150',
+          'mb-1 block h-0.5 w-6 rounded-full transition-opacity duration-150',
           open && 'opacity-0',
+          !open && scrolledHalfway ? 'bg-white' : 'bg-gray-700',
         )}
       ></span>
       <span
         className={clsx(
-          'block h-0.5 w-6 rounded-full transition duration-300',
+          'block h-0.5 w-6 rounded-full transition-transform duration-300',
           open && '-translate-y-1.5 -rotate-45 bg-red-800',
-          !open && 'bg-gray-700',
+          !open && scrolledHalfway ? 'bg-white' : 'bg-gray-700',
         )}
       ></span>
     </button>
