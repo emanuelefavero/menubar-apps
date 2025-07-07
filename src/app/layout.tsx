aae2ac50a83e03528abd836d3e6db54f300c444e
Emanuele Favero <emafaux89@gmail.com>
2025-07-08 01:44:36 +0200

    feat: add overflow background bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
+
+        {/* OverflowBackgroundBottom */}
+        <div
+          className='pointer-events-none fixed right-0 bottom-0 left-0 z-[-9999] h-1/2 bg-(--background)'
+          aria-hidden='true'
+        />
       </body>
     </html>
   )
