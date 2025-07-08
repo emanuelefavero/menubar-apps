0de7da44952b5915c2638713bd039db97d992e9b
Emanuele Favero <emafaux89@gmail.com>
2025-07-08 02:02:24 +0200

    feat: add BackgroundOverflow component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header/Header'
+import BackgroundOverflow from '@/components/shared/BackgroundOverflow'
 import { description, title } from '@/config/metadata'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
@@ -27,20 +28,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* OverflowBackgroundTop */}
-        <div
-          className='pointer-events-none fixed top-0 right-0 left-0 z-[-9999] h-1/2 bg-linear-to-r from-(--bg-image-top-left) to-(--bg-image-top-right)'
-          aria-hidden='true'
-        />
-
         <Header />
         <main>{children}</main>
-
-        {/* OverflowBackgroundBottom */}
-        <div
-          className='pointer-events-none fixed right-0 bottom-0 left-0 z-[-9999] h-1/2 bg-(--background)'
-          aria-hidden='true'
-        />
+        <BackgroundOverflow />
       </body>
     </html>
   )
