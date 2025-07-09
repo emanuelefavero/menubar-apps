e7fddb0007db2e4fbc4646f2485876032d72e40e
Emanuele Favero <emafaux89@gmail.com>
2025-07-09 06:05:25 +0200

    fix: add useScrollPosition and useViewportSize in BackgroundOverflow

--- src/components/shared/BackgroundOverflow/BackgroundOverflow.tsx
+++ src/components/shared/BackgroundOverflow/BackgroundOverflow.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 // Dynamic import for client-side rendering (needed to fix glitchy refresh on page load)
+import { useScrollPosition } from '@/hooks/useScrollPosition'
+import { useViewportSize } from '@/hooks/useViewportSize'
 import dynamic from 'next/dynamic'
 const Top = dynamic(
   () => import('@/components/shared/BackgroundOverflow/Top'),
@@ -18,10 +20,16 @@ const Bottom = dynamic(
 // TIP: This component is used to create a background overflow effect at the top and bottom of the page since the Hero component has a different color at the top compared to the bottom of the page
 
 export default function Component() {
+  const { y } = useScrollPosition()
+  const { height } = useViewportSize()
+
   return (
     <>
-      <Top />
-      <Bottom />
+      {/* Display Top component only when the user has not scrolled more than halfway */}
+      {y < height / 2 && <Top />}
+
+      {/* Display Bottom component only when the user has scrolled more than halfway */}
+      {y > height / 2 && <Bottom />}
     </>
   )
 }
