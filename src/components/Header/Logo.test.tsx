b4ee30821c63af68a542d8c7c37f985d27a93d6d
Emanuele Favero <emafaux89@gmail.com>
2025-07-10 15:10:43 +0200

    test: Logo has correct href attribute

--- src/components/Header/Logo.test.tsx
+++ src/components/Header/Logo.test.tsx
@@ -1,13 +1,20 @@
 import { title } from '@/config/metadata'
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 import Component from './Logo'
 
 describe('Logo', () => {
-  it('renders with correct text', () => {
+  beforeAll(() => {
     render(<Component />)
+  })
 
+  it('renders with correct text', () => {
     const item = screen.getByRole('link', { name: title })
     expect(item.textContent).toBe(title)
+  })
+
+  it('has correct href attribute', () => {
+    const item = screen.getByRole('link', { name: title })
+    expect(item.getAttribute('href')).toBe('/')
   })
 })
