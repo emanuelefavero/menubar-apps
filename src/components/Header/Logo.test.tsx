6ea4fec8424e0765ed929305bb5f4c0f638869b3
Emanuele Favero <emafaux89@gmail.com>
2025-07-10 14:56:31 +0200

    refactor: Logo unit test

--- src/components/Header/Logo.test.tsx
+++ src/components/Header/Logo.test.tsx
@@ -1,13 +1,13 @@
 import { title } from '@/config/metadata'
 import { render, screen } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import Component from './Logo'
 
 describe('Logo', () => {
-  test('renders with correct text', () => {
+  it('renders with correct text', () => {
     render(<Component />)
 
-    const logo = screen.getByRole('link', { name: title })
-    expect(logo.textContent).toBe(title)
+    const item = screen.getByRole('link', { name: title })
+    expect(item.textContent).toBe(title)
   })
 })
