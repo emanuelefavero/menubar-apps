ace64682fc58ab67c6dc33df17aca9eda9f5a15f
Emanuele Favero <emafaux89@gmail.com>
2025-07-11 01:14:25 +0200

    refactor: useIsActiveLink unit test

--- src/hooks/useIsActiveLink.test.tsx
+++ src/hooks/useIsActiveLink.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from '@testing-library/react'
+import { renderHook } from '@testing-library/react'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 import { useIsActiveLink } from './useIsActiveLink'
 
@@ -8,25 +8,15 @@ vi.mock('next/navigation', () => ({
   usePathname: () => mockUsePathname(),
 }))
 
-// Test wrapper component to use the hook
-function ComponentWithHook({ href }: { href: string }) {
-  const isActive = useIsActiveLink(href)
-  return <div data-testid='is-active'>{isActive ? 'true' : 'false'}</div>
-}
-
 describe('useIsActiveLink', () => {
   afterEach(() => {
-    cleanup()
     vi.clearAllMocks()
   })
 
   it('returns true when url matches the href', () => {
-    mockUsePathname.mockReturnValue('/about') // Simulate the current URL
-
-    render(<ComponentWithHook href='/about' />) // href is '/about'
-    const item = screen.getByTestId('is-active')
-
-    expect(item.textContent).toBe('true')
+    mockUsePathname.mockReturnValue('/about') // url
+    const { result } = renderHook(() => useIsActiveLink('/about')) // href
+    expect(result.current).toBe(true)
   })
 
   it('returns false when url does not match the href', () => {
@@ -34,13 +24,9 @@ describe('useIsActiveLink', () => {
 
     nonMatchingRoutes.forEach((route) => {
       mockUsePathname.mockReturnValue(route)
-
-      render(<ComponentWithHook href='/about' />)
-      const item = screen.getByTestId('is-active')
-
-      expect(item.textContent).toBe('false')
-      cleanup() // Clean up after each iteration
-      vi.clearAllMocks() // Clear mocks to reset state
+      const { result } = renderHook(() => useIsActiveLink('/about')) // href
+      expect(result.current).toBe(false)
+      vi.clearAllMocks() // Clear mock for the next iteration
     })
   })
 })
