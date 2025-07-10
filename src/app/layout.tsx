b617be50888206c93da12755719fb33cc0804c38
Emanuele Favero <emafaux89@gmail.com>
2025-07-10 08:28:38 +0200

    fix: remove unused Geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,7 @@ import Header from '@/components/Header/Header'
 import BackgroundOverflow from '@/components/shared/BackgroundOverflow/BackgroundOverflow'
 import { description, title } from '@/config/metadata'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: title,
@@ -27,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header />
         <main>{children}</main>
         <BackgroundOverflow />
