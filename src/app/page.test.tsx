46f4ad45ccf055651d6d2988b8137e59b9df9ba5
Emanuele Favero <emafaux89@gmail.com>
2025-07-10 14:50:31 +0200

    refactor: homepage unit test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,14 +1,17 @@
 import { heroAppName } from '@/config/hero'
 import { render, screen } from '@testing-library/react'
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import Page from './page'
 
-test('renders a heading', () => {
-  render(<Page />)
+describe('Homepage', () => {
+  it('renders a heading', () => {
+    render(<Page />)
 
-  const heading = screen.getByRole('heading', {
-    name: new RegExp(heroAppName, 'i'),
+    const heading = screen.getByRole('heading', {
+      level: 1,
+      name: new RegExp(heroAppName, 'i'),
+    })
+
+    expect(heading).toBeDefined()
   })
-
-  expect(heading).toBeDefined()
 })
