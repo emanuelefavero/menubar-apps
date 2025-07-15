4e290577e2a7cb49019d8bffb79fe23b35217c70
Emanuele Favero <emafaux89@gmail.com>
2025-07-15 18:05:49 +0200

    feat: add foreground-secondary css color variable

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 export default function Component() {
   const year = new Date().getFullYear()
+
   return (
     <footer className='mt-8 flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
       <div className='flex w-full flex-wrap items-center justify-center gap-4'>
@@ -10,13 +11,15 @@ export default function Component() {
           <span className='text-center font-semibold 4xs:text-left'>
             {title}
           </span>
-          <span className='hidden opacity-80 4xs:inline-block'>
+          <span className='hidden text-(--foreground-secondary) 4xs:inline-block'>
             &copy; {year}
           </span>
         </span>
         <span className='mx-2 hidden h-4 w-px rounded bg-white/40 xs:inline-block dark:bg-slate-400/40'></span>
         <span className='text-center text-xs'>
-          <span className='opacity-80 mr-0.5'>Developed by </span>
+          <span className='mr-0.5 text-(--foreground-secondary)'>
+            Developed by{' '}
+          </span>
           <Link
             href={devUrl}
             className='font-semibold underline-offset-2 hover:underline'
