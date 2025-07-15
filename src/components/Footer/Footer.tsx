cb8b8297a1563cbc601d9240f6ab0b6813c17a1e
Emanuele Favero <emafaux89@gmail.com>
2025-07-15 17:59:04 +0200

    feat: change footer colors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,20 +4,22 @@ import Link from 'next/link'
 export default function Component() {
   const year = new Date().getFullYear()
   return (
-    <footer className='mt-8 flex w-full flex-col items-center justify-center rounded-xl border border-white/20 bg-white/30 px-4 py-6 text-neutral-700 shadow-lg backdrop-blur 5xs:px-6 dark:bg-slate-800/30 dark:text-neutral-300'>
+    <footer className='mt-8 flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
       <div className='flex w-full flex-wrap items-center justify-center gap-4'>
-        <span className='flex items-center gap-2 text-sm font-semibold'>
-          <span className='text-center 4xs:text-left'>{title}</span>
-          <span className='hidden opacity-70 4xs:inline-block'>
+        <span className='flex items-center gap-2 text-sm'>
+          <span className='text-center font-semibold 4xs:text-left'>
+            {title}
+          </span>
+          <span className='hidden opacity-80 4xs:inline-block'>
             &copy; {year}
           </span>
         </span>
         <span className='mx-2 hidden h-4 w-px rounded bg-white/40 xs:inline-block dark:bg-slate-400/40'></span>
         <span className='text-center text-xs'>
-          Developed by{' '}
+          <span className='opacity-80 mr-0.5'>Developed by </span>
           <Link
             href={devUrl}
-            className='underline-offset-2 hover:underline'
+            className='font-semibold underline-offset-2 hover:underline'
             target='_blank'
             rel='noopener noreferrer'
             aria-label={`Visit ${devName}'s website`}
