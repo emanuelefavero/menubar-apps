7ef8cc9ac15a7e4e2ecd8ee117f8db54b189e6d2
Emanuele Favero <emafaux89@gmail.com>
2025-07-15 18:12:03 +0200

    fix(footer): divider color

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,6 +7,7 @@ export default function Component() {
   return (
     <footer className='mt-8 flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
       <div className='flex w-full flex-wrap items-center justify-center gap-4'>
+        {/* Logo */}
         <span className='flex items-center gap-2 text-sm'>
           <span className='text-center font-semibold 4xs:text-left'>
             {title}
@@ -15,7 +16,11 @@ export default function Component() {
             &copy; {year}
           </span>
         </span>
-        <span className='mx-2 hidden h-4 w-px rounded bg-white/40 xs:inline-block dark:bg-slate-400/40'></span>
+
+        {/* Divider */}
+        <span className='mx-2 hidden h-4 w-px rounded bg-(--foreground-secondary) select-none xs:inline-block'></span>
+
+        {/* Developer Link */}
         <span className='text-center text-xs'>
           <span className='mr-0.5 text-(--foreground-secondary)'>
             Developed by{' '}
