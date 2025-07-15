b2b446996f11fd949ce5945c990f196230c8f7c2
Emanuele Favero <emafaux89@gmail.com>
2025-07-15 19:26:28 +0200

    feat: remove margin top from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo'
 
 export default function Component() {
   return (
-    <footer className='mt-8 flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
+    <footer className='flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
       <div className='flex w-full flex-wrap items-center justify-center gap-4'>
         <Logo />
         <Divider />
