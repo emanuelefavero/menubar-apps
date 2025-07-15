ba08db3134561a175d1d223665402d9bf5b59fe6
Emanuele Favero <emafaux89@gmail.com>
2025-07-15 17:41:14 +0200

    feat: make footer responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,14 +4,16 @@ import Link from 'next/link'
 export default function Component() {
   const year = new Date().getFullYear()
   return (
-    <footer className='mt-8 flex w-full items-center justify-center rounded-xl border border-white/20 bg-white/30 px-6 py-4 text-neutral-700 shadow-lg backdrop-blur dark:bg-slate-800/30 dark:text-neutral-300'>
+    <footer className='mt-8 flex w-full flex-col items-center justify-center rounded-xl border border-white/20 bg-white/30 px-4 py-6 text-neutral-700 shadow-lg backdrop-blur 5xs:px-6 dark:bg-slate-800/30 dark:text-neutral-300'>
       <div className='flex w-full flex-wrap items-center justify-center gap-4'>
         <span className='flex items-center gap-2 text-sm font-semibold'>
-          <span>{title}</span>
-          <span className='opacity-70'>&copy; {year}</span>
+          <span className='text-center 4xs:text-left'>{title}</span>
+          <span className='hidden opacity-70 4xs:inline-block'>
+            &copy; {year}
+          </span>
         </span>
-        <span className='mx-2 h-4 w-px rounded bg-white/40 dark:bg-slate-400/40'></span>
-        <span className='text-xs'>
+        <span className='mx-2 hidden h-4 w-px rounded bg-white/40 xs:inline-block dark:bg-slate-400/40'></span>
+        <span className='text-center text-xs'>
           Developed by{' '}
           <Link
             href={devUrl}
