c9d9ce6bf4e493d568ff7f4024d5c471dee262cf
Emanuele Favero <emafaux89@gmail.com>
2025-07-16 18:15:29 +0200

    test: add footer logo unit test

---
+++ src/components/Footer/Logo.test.tsx
@@ -0,0 +1,20 @@
+import { title } from '@/config/metadata'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import Component from './Logo'
+
+describe('Logo', () => {
+  it('renders the correct year', () => {
+    render(<Component />)
+    const year = new Date().getFullYear().toString()
+    expect(screen.getByText((content) => content.includes(year))).toBeDefined()
+  })
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('renders the correct title from metadata', () => {
+    render(<Component />)
+    expect(screen.getByText(title)).toBeDefined()
+  })
+})
