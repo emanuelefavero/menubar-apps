668393648900266d5e9725833b5e3c8ae9d360bb
Emanuele Favero <emafaux89@gmail.com>
2025-07-16 18:49:03 +0200

    test: add footer divider unit test

---
+++ src/components/Footer/Divider.test.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import Component from './Divider'
+
+describe('Divider', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('is hidden on < xs screens', () => {
+    render(<Component />)
+
+    const item =
+      screen.getByRole('presentation', { hidden: true }) ||
+      document.querySelector('span')
+
+    expect(item).toBeDefined()
+    expect(item?.className).toContain('hidden')
+    expect(item?.className).toContain('xs:inline-block')
+  })
+})
--- src/components/Footer/Divider.tsx
+++ src/components/Footer/Divider.tsx
@@ -1,5 +1,9 @@
 export default function Divider() {
   return (
-    <span className='mx-2 hidden h-4 w-px rounded bg-(--foreground-secondary) select-none xs:inline-block'></span>
+    <span
+      className='mx-2 hidden h-4 w-px rounded bg-(--foreground-secondary) select-none xs:inline-block'
+      role='presentation'
+      aria-hidden='true'
+    ></span>
   )
 }
