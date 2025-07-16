3ccdc9a1ebcb4b38b59ec4b0f3b2163ca6b8acb9
Emanuele Favero <emafaux89@gmail.com>
2025-07-16 18:37:12 +0200

    test: add footer developer link unit test

---
+++ src/components/Footer/DeveloperLink.test.tsx
@@ -0,0 +1,27 @@
+import { devName, devUrl } from '@/config/metadata'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import Component from './DeveloperLink'
+
+describe('DeveloperLink', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('renders developer name correctly', () => {
+    render(<Component />)
+
+    const item = screen.getByText(devName)
+    expect(item).toBeDefined()
+  })
+
+  it('renders developer url correctly', () => {
+    render(<Component />)
+
+    const item = screen.getByRole('link', {
+      name: `Visit ${devName}'s website`,
+    })
+
+    expect(item.getAttribute('href')).toBe(devUrl)
+  })
+})
