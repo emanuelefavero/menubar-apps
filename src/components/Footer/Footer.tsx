c42b4145c49dc43f187f980b83283adeaf14f27d
Emanuele Favero <emafaux89@gmail.com>
2025-07-18 03:22:22 +0200

    refactor: merge top and botto footer sections

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 import DeveloperLink from './DeveloperLink'
 import Divider from './Divider'
@@ -6,31 +7,37 @@ import Logo from './Logo'
 export default function Component() {
   return (
     <footer className='flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
-      <Top />
-      <Bottom />
+      <Row>
+        <Link href='terms-of-use' className='text-sm font-medium uppercase'>
+          Terms of Use
+        </Link>
+        <Link href='privacy-policy' className='text-sm font-medium uppercase'>
+          Privacy Policy
+        </Link>
+      </Row>
+      <Row>
+        <Logo />
+        <Divider />
+        <DeveloperLink />
+      </Row>
     </footer>
   )
 }
 
-function Top() {
-  return (
-    <div className='flex w-full flex-wrap items-center justify-center gap-4 py-2'>
-      <Link href='terms-of-use' className='text-sm font-medium uppercase'>
-        Terms of Use
-      </Link>
-      <Link href='privacy-policy' className='text-sm font-medium uppercase'>
-        Privacy Policy
-      </Link>
-    </div>
-  )
+interface FooterRowProps {
+  children: React.ReactNode
+  className?: string
 }
 
-function Bottom() {
+function Row({ children, className }: FooterRowProps) {
   return (
-    <div className='flex w-full flex-wrap items-center justify-center gap-4 py-2'>
-      <Logo />
-      <Divider />
-      <DeveloperLink />
+    <div
+      className={clsx(
+        'flex w-full flex-wrap items-center justify-center gap-4 py-2',
+        className,
+      )}
+    >
+      {children}
     </div>
   )
 }
