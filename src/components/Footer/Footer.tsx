19fc1cb6bee5a9eded0142d3c254e218e1ad38b8
Emanuele Favero <emafaux89@gmail.com>
2025-07-18 04:13:54 +0200

    feat: add rounded state to footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import DeveloperLink from './DeveloperLink'
 import Divider from './Divider'
 import Link from './Link'
@@ -13,8 +14,15 @@ export default function Component() {
   return (
     <footer className='flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
       <Row>
-        {links.map((link) => (
-          <Link key={`footer-link-${link.href}`} href={link.href}>
+        {links.map((link, index) => (
+          <Link
+            key={`footer-link-${link.href}`}
+            href={link.href}
+            className={clsx(
+              index === 0 ? 'rounded-l-full' : '',
+              index === links.length - 1 ? 'rounded-r-full' : '',
+            )}
+          >
             {link.label}
           </Link>
         ))}
