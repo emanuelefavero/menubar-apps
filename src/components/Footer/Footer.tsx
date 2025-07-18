aaf02d97307fe62c4be290c3b531a5adf2a16261
Emanuele Favero <emafaux89@gmail.com>
2025-07-18 03:18:57 +0200

    feat: add top and bottom footer sections

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import DeveloperLink from './DeveloperLink'
 import Divider from './Divider'
 import Logo from './Logo'
@@ -5,11 +6,31 @@ import Logo from './Logo'
 export default function Component() {
   return (
     <footer className='flex w-full flex-col items-center justify-center bg-(--background) px-4 py-6 text-(--foreground) 5xs:px-6'>
-      <div className='flex w-full flex-wrap items-center justify-center gap-4'>
-        <Logo />
-        <Divider />
-        <DeveloperLink />
-      </div>
+      <Top />
+      <Bottom />
     </footer>
+  )
+}
+
+function Top() {
+  return (
+    <div className='flex w-full flex-wrap items-center justify-center gap-4 py-2'>
+      <Link href='terms-of-use' className='text-sm font-medium uppercase'>
+        Terms of Use
+      </Link>
+      <Link href='privacy-policy' className='text-sm font-medium uppercase'>
+        Privacy Policy
+      </Link>
+    </div>
+  )
+}
+
+function Bottom() {
+  return (
+    <div className='flex w-full flex-wrap items-center justify-center gap-4 py-2'>
+      <Logo />
+      <Divider />
+      <DeveloperLink />
+    </div>
   )
 }
