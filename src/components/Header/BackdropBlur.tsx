71add458520f84831ae0381fd2c19bc50b368c3e
Emanuele Favero <emafaux89@gmail.com>
2025-07-21 00:11:05 +0200

    refactor: move Props interface beside component

--- src/components/Header/BackdropBlur.tsx
+++ src/components/Header/BackdropBlur.tsx
@@ -2,11 +2,6 @@ import clsx from 'clsx'
 
 // TIP: Use this component to create a backdrop blur effect when you can't use the `backdrop-blur` utility directly on the element, as CSS does not support nested backdrop-blur effects. @see https://github.com/tailwindlabs/tailwindcss/discussions/15103
 
-interface Props {
-  className?: string
-  blur?: 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl'
-}
-
 const blurClasses = {
   none: 'before:backdrop-blur-none',
   sm: 'before:backdrop-blur-sm',
@@ -15,6 +10,11 @@ const blurClasses = {
   xl: 'before:backdrop-blur-xl',
   '2xl': 'before:backdrop-blur-2xl',
   '3xl': 'before:backdrop-blur-3xl',
+}
+
+interface Props {
+  className?: string
+  blur?: 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl'
 }
 
 export default function Component({ className, blur = 'md' }: Props) {
