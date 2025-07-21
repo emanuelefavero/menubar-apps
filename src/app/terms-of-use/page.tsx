a5a70d692ac5e95bb51738e808d1aa9497cfebe7
Emanuele Favero <emafaux89@gmail.com>
2025-07-21 18:35:06 +0200

    feat: style a tag inside markdown element

--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
     <>
       <article
         dangerouslySetInnerHTML={{ __html: html }}
-        className='prose max-w-screen-lg px-4 py-24 lg:prose-lg dark:prose-invert'
+        className='prose max-w-screen-lg px-4 py-24 lg:prose-lg dark:prose-invert prose-a:underline-offset-4 prose-a:transition prose-a:duration-250 prose-a:hover:text-(--foreground-secondary) prose-a:active:scale-[0.97]'
       />
     </>
   )
