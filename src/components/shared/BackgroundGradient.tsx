fdb646501663dbaa3c5e4c1e5da5c531db309e3d
Emanuele Favero <emafaux89@gmail.com>
2025-07-28 03:25:38 +0200

    feat: move radial gradient code to tailwind

--- src/components/shared/BackgroundGradient.tsx
+++ src/components/shared/BackgroundGradient.tsx
@@ -5,6 +5,10 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   className?: string
 }
 
+// sky-200
+// blue-400
+// indigo-900
+
 export default function Component({
   zIndex = 'z-0',
   className = '',
@@ -14,13 +18,9 @@ export default function Component({
     <div
       className={clsx(
         zIndex,
-        'pointer-events-none fixed inset-0 opacity-10 mix-blend-multiply select-none dark:opacity-10 dark:mix-blend-screen',
+        'pointer-events-none fixed inset-0 bg-radial-[at_50%_53%] from-transparent via-blue-800/20 to-sky-400 to-100% opacity-10 mix-blend-multiply select-none dark:opacity-10 dark:mix-blend-screen',
         className,
       )}
-      style={{
-        background:
-          'radial-gradient(circle, rgba(0, 0, 0, 0) 0%, rgba(10, 40, 97, 0.2) 50%, rgba(39, 192, 243, 1) 100%)',
-      }}
       aria-hidden='true'
       {...props}
     />
