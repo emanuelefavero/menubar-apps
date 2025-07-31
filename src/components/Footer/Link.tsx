b385058963951e7254b15863cf6ad277362fc7cc
Emanuele Favero <emafaux89@gmail.com>
2025-07-31 06:41:21 +0200

    feat: set backdrop blur only on active footer link

--- src/components/Footer/Link.tsx
+++ src/components/Footer/Link.tsx
@@ -20,14 +20,16 @@ export default function Component({
   const isActive = useIsActiveLink(href)
 
   const baseStyles = clsx(
-    'h-full px-3 py-2 text-sm font-medium select-none uppercase no-underline transition duration-250 backdrop-blur-[6px] border border-transparent active:scale-[0.97]',
+    'h-full px-3 py-2 text-sm font-medium select-none uppercase no-underline transition duration-250 border border-transparent active:scale-[0.97]',
   )
 
   const activeStyles = clsx(
-    'border-dotted border-white bg-white/20 dark:border-white/15 dark:bg-white/[0.025]',
+    'backdrop-blur-[6px] border-dotted border-white bg-white/20 dark:border-white/15 dark:bg-white/[0.025]',
   )
 
-  const hoverStyles = clsx('hover:bg-white/50 dark:hover:bg-white/5')
+  const hoverStyles = clsx(
+    'hover:backdrop-blur-[6px] hover:bg-white/50 dark:hover:bg-white/5',
+  )
 
   return (
     <Link
