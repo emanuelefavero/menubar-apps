9fbe210c55519962289834c113e9d97610c0d4db
Emanuele Favero <emafaux89@gmail.com>
2025-07-31 06:26:37 +0200

    feat: prevent text selection in footer links

--- src/components/Footer/Link.tsx
+++ src/components/Footer/Link.tsx
@@ -20,7 +20,7 @@ export default function Component({
   const isActive = useIsActiveLink(href)
 
   const baseStyles = clsx(
-    'h-full px-3 py-2 text-sm font-medium uppercase no-underline transition duration-250 backdrop-blur-[6px] border border-transparent active:scale-[0.97]',
+    'h-full px-3 py-2 text-sm font-medium select-none uppercase no-underline transition duration-250 backdrop-blur-[6px] border border-transparent active:scale-[0.97]',
   )
 
   const activeStyles = clsx(
