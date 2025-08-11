0264f1132032f84d81b01b53e7eaf9f1e50f9d29
Emanuele Favero <emafaux89@gmail.com>
2025-08-11 05:20:24 +0200

    feat: add class-variance-authority

--- src/components/Footer/Row.tsx
+++ src/components/Footer/Row.tsx
@@ -1,25 +1,30 @@
 import { cn } from '@/lib/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-interface Props {
-  children: React.ReactNode
-  variant?: 'top' | 'bottom'
-  className?: string
-}
+const variants = cva('flex w-full flex-wrap items-center justify-center', {
+  variants: {
+    variant: {
+      top: 'gap-2 xs:gap-0',
+      bottom: 'gap-4 py-2',
+    },
+  },
+  defaultVariants: {
+    variant: 'top',
+  },
+})
+
+interface Props
+  extends React.ComponentPropsWithRef<'div'>,
+    VariantProps<typeof variants> {}
 
 export default function Component({
   children,
-  variant = 'top',
+  variant,
   className,
+  ...props
 }: Props) {
   return (
-    <div
-      className={cn(
-        'flex w-full flex-wrap items-center justify-center',
-        variant === 'bottom' && 'gap-4 py-2',
-        variant === 'top' && 'gap-2 xs:gap-0',
-        className,
-      )}
-    >
+    <div className={cn(variants({ variant }), className)} {...props}>
       {children}
     </div>
   )
