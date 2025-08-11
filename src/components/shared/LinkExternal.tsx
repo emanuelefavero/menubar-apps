211f24427b30bda370e41ec5f30d0f392256c1e4
Emanuele Favero <emafaux89@gmail.com>
2025-08-11 10:52:10 +0200

    refactor: LinkExternal

--- src/components/shared/LinkExternal.tsx
+++ src/components/shared/LinkExternal.tsx
@@ -1,27 +1,18 @@
 import { cn } from '@/lib/utils'
 import { focusStyle } from '@/styles/focus'
-import type { Href } from '@/types/href'
 
-interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  href: Href
-  children: React.ReactNode
-  className?: string
-}
+type Props = React.ComponentPropsWithRef<'a'>
 
-export default function Component({
-  href,
-  children,
-  className = '',
-  ...props
-}: Props) {
+export default function Component({ children, className, ...props }: Props) {
   const baseStyles = cn(
     'font-medium underline underline-offset-4 rounded-full px-1 py-0.5 transition duration-250 hover:text-(--foreground-secondary) active:scale-[0.97]',
+
+    // Focus
     `focus-visible:no-underline ${focusStyle}`,
   )
 
   return (
     <a
-      href={href}
       className={cn(baseStyles, className)}
       target='_blank'
       rel='noopener noreferrer'
