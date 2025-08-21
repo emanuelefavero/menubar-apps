bc838bbd642c33f691be7c1c8d42b4f56afff433
Emanuele Favero <emafaux89@gmail.com>
2025-08-21 02:50:02 +0200

    feat: add cx utility for lighteight className merging

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+// * cn
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// * cx
+// TIP: use it in client components when you don't need complex merging or class name deduplication.
+export type CxClassValue = string | null | undefined | false | CxClassValue[]
+export function cx(...inputs: CxClassValue[]) {
+  return inputs
+    .flat()
+    .filter((x) => typeof x === 'string')
+    .join(' ')
+    .trim()
 }
